71678b70fc2e30c7282fa0d44b0a65b0fcd05075
XandRules <alexandresilva58@gmail.com>
2020-06-02 23:53:23 -0300

    criado rota create e upload do freelancer

--- backend/src/app/controllers/FreelancerController.js
+++ backend/src/app/controllers/FreelancerController.js
@@ -1,7 +1,27 @@
+import * as Yup from 'yup';
+
 import Freelancer from '../models/freelancer';
 
 class FreelancerController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      cpf: Yup.string().required(),
+      phone: Yup.string().required(),
+      gender: Yup.string().required(),
+      latitude: Yup.string(),
+      longitude: Yup.string(),
+      birth: Yup.string().required(),
+      active: Yup.boolean().default(false),
+      terms_of_use: Yup.boolean(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json('Validation fail');
+    }
+
     const freelancerExists = await Freelancer.findOne({
       where: { email: req.body.email },
     });
@@ -10,9 +30,70 @@ class FreelancerController {
       return res.status(400).json({ error: 'Freelancer already exists.' });
     }
 
-    const { id, name, email, active, cpf, phone } = await Freelancer.create(
-      req.body
-    );
+    let newFreelancer = null;
+
+    try {
+      newFreelancer = await Freelancer.create(req.body);
+    } catch (error) {
+      return res.status(401).json({ error: error.name });
+    }
+
+    const { id, name, email, active, cpf, phone } = newFreelancer;
+
+    return res.json({
+      id,
+      name,
+      email,
+      active,
+      cpf,
+      phone,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string(),
+      phone: Yup.string(),
+      gender: Yup.string(),
+      latitude: Yup.string(),
+      longitude: Yup.string(),
+      birth: Yup.string(),
+      terms_of_use: Yup.boolean(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json('Validation fail');
+    }
+
+    const { email, oldPassword } = req.body;
+
+    const freelancer = await Freelancer.findByPk(req.userId);
+
+    if (email !== freelancer.email) {
+      const freelancerExists = await Freelancer.findOne({
+        where: { email },
+      });
+
+      if (freelancerExists) {
+        return res.status(400).json({ error: 'Freelancer already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await freelancer.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, active, cpf, phone } = await freelancer.update(req.body);
 
     return res.json({
       id,
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,21 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import FreelancerController from './app/controllers/FreelancerController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
+
+const upload = multer(multerConfig);
 
 routes.post('/freelancers', FreelancerController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/freelancers', FreelancerController.update);
 
 export default routes;
