f6c8e850afebb575575d56d29e87ae5e3fd8cc36
xandrules <alexandresilva58@gmail.com>
2020-09-19 16:54:54 -0500

    add pd

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,23 @@
-module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'easyjob',
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
+// module.exports = {
+//   dialect: 'postgres',
+//   host: 'localhost',
+//   username: 'postgres',
+//   password: 'docker',
+//   database: 'easyjob',
+//   define: {
+//     timestamps: true,
+//     underscored: true,
+//     underscoredAll: true,
+//   },
+// };
+
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
   },
-};
+});
+
+client.connect();
