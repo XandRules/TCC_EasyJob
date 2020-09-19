ba529a9c8fdf7d7807666f9a6c53a15e4f0a8584
xandrules <alexandresilva58@gmail.com>
2020-09-19 17:51:22 -0500

    configurando deploy

--- src/config/database.js
+++ src/config/database.js
@@ -21,3 +21,16 @@ const client = new Client({
 });
 
 client.connect();
+
+module.exports = {
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
