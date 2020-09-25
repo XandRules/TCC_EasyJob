230d2461cd8565f9b77231ddba86b90986329a30
XandRules <alexandresilva58@gmail.com>
2020-09-25 16:19:57 -0300

    alterado o model de freelancer

--- src/app/models/freelancer.js
+++ src/app/models/freelancer.js
@@ -1,30 +1,29 @@
-import Sequelize, { Model } from 'sequelize';
+import Sequelize, {
+  Model
+} from 'sequelize';
 
 import bcrypt from 'bcryptjs';
 
 class Freelancer extends Model {
   static init(sequelize) {
     // migration de freelancer
-    super.init(
-      {
-        name: Sequelize.STRING,
-        email: Sequelize.STRING,
-        cpf: Sequelize.STRING,
-        phone: Sequelize.STRING,
-        password: Sequelize.VIRTUAL,
-        password_hash: Sequelize.STRING,
-        latitude: Sequelize.STRING,
-        longitude: Sequelize.STRING,
-        gender: Sequelize.STRING,
-        birth: Sequelize.STRING,
-        active: Sequelize.STRING,
-        terms_of_use: Sequelize.STRING,
-        speciality_id: Sequelize.NUMBER,
-      },
-      {
-        sequelize,
-      }
-    );
+    super.init({
+      name: Sequelize.STRING,
+      email: Sequelize.STRING,
+      password_hash: Sequelize.STRING,
+      phone: Sequelize.STRING,
+      cpf: Sequelize.STRING,
+      bio: Sequelize.STRING,
+      birth: Sequelize.STRING,
+      gender: Sequelize.STRING,
+      active: Sequelize.STRING,
+      terms_of_use: Sequelize.STRING,
+      password: Sequelize.VIRTUAL,
+      speciality_id: Sequelize.NUMBER,
+      avatar_id: Sequelize.NUMBER,
+    }, {
+      sequelize,
+    });
 
     this.addHook('beforeSave', async (freelancer) => {
       if (freelancer.password) {
@@ -36,7 +35,10 @@ class Freelancer extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
+    this.belongsTo(models.File, {
+      foreignKey: 'avatar_id',
+      as: 'avatar'
+    });
   }
 
   checkPassword(password) {
