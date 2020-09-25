a79a86774f7c6f92bf25302910a44242570521f0
xandrules <alexandresilva58@gmail.com>
2020-09-25 17:50:25 -0500

    deploy

--- src/server.js
+++ src/server.js
@@ -1,35 +1,4 @@
 import app from './app';
 
-var http = require('https').createServer(app);
-var io = require('socket.io')(http);
-
-app.get('/', (req, res) => {
-  res.send("Servidor iniciado com sucesso");
-})
-
-var clients = {};
-io.origins('*:*');
-
-io.on("connection", (client) => {
-  client.on("join", (name) => {
-
-    console.log("joined" + name);
-    clients[client.id] = name;
-    client.emit("update", "You have connected to the server");
-    client.broadcast.emit("update", name + "has joined the server");
-  });
-
-  client.on("send", function (msg) {
-    console.log("Message: " + msg);
-    client.broadcast.emit("chat", clients[client.id], msg);
-  });
-
-  client.on("disconnect", function () {
-    console.log("Disconnect");
-    io.emit("update", clients[client.id] + " has left the server.");
-    delete clients[client.id];
-  });
-
-});
 
 app.listen(process.env.PORT || 3333);
