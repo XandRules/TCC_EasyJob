a505148916b9e6248ec7bbd040222de377ec2779
XandRules <alexandresilva58@gmail.com>
2020-09-28 17:13:29 -0300

    deploy

--- src/config/mail.js
+++ src/config/mail.js
@@ -13,7 +13,7 @@ export default {
       "clientId": "128826042594-0b0ade1t7o4afj4fcl629tvr6irqt48t.apps.googleusercontent.com",
       "clientSecret": "onRadfB7cixkdg87h3VcQKHN",
       "refreshToken": "1//0495bFq0hLR1bCgYIARAAGAQSNgF-L9Ir9HOyqKj93gYFtwCXT7AFktGtSQxTlhgc7RbFLDpojLYBL1P6d4xp4bsraG8tUNkUUw",
-      "accessToken": "ya29.a0AfH6SMAaAj3xoxJcc24_YSok01_Zyj_9dhDbtNaPao1X1Mbe4G5oMJgm2rVXhQozgfFSOLiKh2i1y4tzR0CQBHxpQlvIRKdvqq4xnBvc4ToGSlzr5nSaTHtqFrh_RqeJzIIiBLMN3iWRfnjYqjOMjdtLV9q4iEQxXcI"
+      "accessToken": "ya29.a0AfH6SMCgsnWNJGU-w_Mw5J0jtfI0NFC1TF-dOh-zA0-uf9YLA_MAkZEErU-kHqtokRV_TDKfeMOFiE_8HgvNpnENCyCPZKBTMpaprz2pXcFr33wzvVwOmwkfyCsc8BKL8lce7TR5jiKbzT8wp-QOJMoyMKCgqpWV0vj7"
     }
   }
 };
