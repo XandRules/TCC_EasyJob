d7b5b4dbfb7bd76339d92d789b32ba17856a6f96
XandRules <alexandresilva58@gmail.com>
2020-10-02 15:50:45 -0300

    retorno de nome do estabelecimento

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -7,19 +7,33 @@ import Establishment from '../models/Establishment';
 
 class SessionController {
   async storeFreelancer(req, res) {
-    const { email, password } = req.body;
+    const {
+      email,
+      password
+    } = req.body;
 
-    const freelancer = await Freelancer.findOne({ where: { email } });
+    const freelancer = await Freelancer.findOne({
+      where: {
+        email
+      }
+    });
 
     if (!freelancer) {
-      return res.json({ error: 'Freelancer not found' });
+      return res.json({
+        error: 'Freelancer not found'
+      });
     }
 
     if (!(await freelancer.checkPassword(password))) {
-      return res.json({ error: 'Password does not match' });
+      return res.json({
+        error: 'Password does not match'
+      });
     }
 
-    const { id, name } = freelancer;
+    const {
+      id,
+      name
+    } = freelancer;
 
     const role = 'freelancer';
 
@@ -30,37 +44,57 @@ class SessionController {
         email,
         role,
       },
-      token: jwt.sign({ id, email }, authConfig.secret, {
+      token: jwt.sign({
+        id,
+        email
+      }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
       }),
     });
   }
 
   async storeEstablishment(req, res) {
-    const { email, password } = req.body;
+    const {
+      email,
+      password
+    } = req.body;
 
-    const establishment = await Establishment.findOne({ where: { email } });
+    const establishment = await Establishment.findOne({
+      where: {
+        email
+      }
+    });
 
     if (!establishment) {
-      return res.json({ error: 'Establishment not found' });
+      return res.json({
+        error: 'Establishment not found'
+      });
     }
 
     if (!(await establishment.checkPassword(password))) {
-      return res.json({ error: 'Password does not match' });
+      return res.json({
+        error: 'Password does not match'
+      });
     }
 
-    const { id, name } = establishment;
+    const {
+      id,
+      company_name
+    } = establishment;
 
     const role = 'establish';
 
     return res.json({
       establishment: {
         id,
-        name,
+        name: company_name,
         email,
         role,
       },
-      token: jwt.sign({ id, email }, authConfig.secret, {
+      token: jwt.sign({
+        id,
+        email
+      }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
       }),
     });
