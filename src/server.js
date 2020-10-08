b89ce4a7df532bad78c8b70aab869f9d9d417ec6
XandRules <alexandresilva58@gmail.com>
2020-10-08 16:45:25 -0300

    deploy

--- src/server.js
+++ src/server.js
@@ -1,4 +1,34 @@
 import app from './app';
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.listen(process.env.PORT || 3333);
+var clients = {}; 
+
+app.get('/', function(req, res){
+  res.send('server is running');
+});
+
+io.on("connection", function (client) {  
+    client.on("join", function(name){
+    	console.log("Joined: " + name);
+        clients[client.id] = name;
+        client.emit("update", "You have connected to the server.");
+        client.broadcast.emit("update", name + " has joined the server.")
+    });
+
+    client.on("send", function(msg){
+    	console.log("Message: " + msg);
+        client.broadcast.emit("chat", clients[client.id], msg);
+    });
+
+    client.on("disconnect", function(){
+    	console.log("Disconnect");
+        io.emit("update", clients[client.id] + " has left the server.");
+        delete clients[client.id];
+    });
+});
+
+var server  = app.listen(process.env.PORT || 3333);
+
+io.listen(server);
