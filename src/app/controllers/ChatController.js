203b59f913eebc7236e7899cc7ed0bbe0c634e19
XandRules <alexandresilva58@gmail.com>
2020-10-14 16:24:54 -0300

    deploy

--- src/app/controllers/ChatController.js
+++ src/app/controllers/ChatController.js
@@ -1,38 +1,7 @@
-import app from '../../app';
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
 class ChatController {
-    async index(req, res) {    
-    var clients = {}; 
-    
-    app.get('/', function(req, res){
-      res.send('server is running');
-    });
-    
-    io.on("connection", function (client) {  
-        client.on("join", function(name){
-          console.log("Joined: " + name);
-            clients[client.id] = name;
-            client.emit("update", "You have connected to the server.");
-            client.broadcast.emit("update", name + " has joined the server.")
-        });
-    
-        client.on("send", function(msg){
-          console.log("Message: " + msg);
-            client.broadcast.emit("chat", clients[client.id], msg);
-        });
-    
-        client.on("disconnect", function(){
-          console.log("Disconnect");
-            io.emit("update", clients[client.id] + " has left the server.");
-            delete clients[client.id];
-        });
-    });
-    
-    var server  = app.listen(process.env.PORT || 3333);
-    
-    io.listen(server);
+
+  async index(req, res) {
+    res.send('server is running');
   }
 
   async create(req, res) {
--- src/server.js
+++ src/server.js
@@ -1,34 +1,34 @@
 import app from './app';
 
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-// var clients = {}; 
+var clients = {}; 
 
-// app.get('/', function(req, res){
-//   res.send('server is running');
-// });
+app.get('/', function(req, res){
+  res.send('server is running');
+});
 
-// io.on("connection", function (client) {  
-//     client.on("join", function(name){
-//     	console.log("Joined: " + name);
-//         clients[client.id] = name;
-//         client.emit("update", "You have connected to the server.");
-//         client.broadcast.emit("update", name + " has joined the server.")
-//     });
+io.on("connection", function (client) {  
+    client.on("join", function(name){
+    	console.log("Joined: " + name);
+        clients[client.id] = name;
+        client.emit("update", "You have connected to the server.");
+        client.broadcast.emit("update", name + " has joined the server.")
+    });
 
-//     client.on("send", function(msg){
-//     	console.log("Message: " + msg);
-//         client.broadcast.emit("chat", clients[client.id], msg);
-//     });
+    client.on("send", function(msg){
+    	console.log("Message: " + msg);
+        client.broadcast.emit("chat", clients[client.id], msg);
+    });
 
-//     client.on("disconnect", function(){
-//     	console.log("Disconnect");
-//         io.emit("update", clients[client.id] + " has left the server.");
-//         delete clients[client.id];
-//     });
-// });
+    client.on("disconnect", function(){
+    	console.log("Disconnect");
+        io.emit("update", clients[client.id] + " has left the server.");
+        delete clients[client.id];
+    });
+});
 
 var server  = app.listen(process.env.PORT || 3333);
 
-// io.listen(server);
+io.listen(server);
