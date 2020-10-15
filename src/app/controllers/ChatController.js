adc8ea56ade36d062f9c962cb0abe388e2da2ef3
XandRules <alexandresilva58@gmail.com>
2020-10-15 15:57:53 -0300

    apenas testes

--- src/app/controllers/ChatController.js
+++ src/app/controllers/ChatController.js
@@ -1,4 +1,7 @@
-import Freelancere from '../models/freelancer'
+import Freelancer from '../models/freelancer'
+import Chat from '../models/Chat'
+
+import * as Yup from 'yup';
 
 class ChatController {
 
@@ -8,7 +11,38 @@ class ChatController {
 
   async create(req, res) {
 
-    return res.json({"connected": `server is running ${req.body.room}`});
+    try {
+
+      const schema = Yup.object().shape({
+        room: Yup.string().required(),
+        freelancer_id: Yup.number().required(),
+        establish_id: Yup.number().required(),
+        announcement_id: Yup.number().required(),
+        message: Yup.string(),
+      });
+
+      await schema.validate(req.body, {
+        abortEarly: false,
+      });
+
+      let chat = null;
+      try {
+        chat = await Chat.create(req.body);
+      } catch (error) {
+        return res.json({
+          error
+        });
+      }
+
+      return res.json(chat);      
+  } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        console.log(error);
+        return res.json({
+          "error": error
+        });
+      }
+    }
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ var io = require('socket.io')(http);
 
 var clients = {}; 
 
+var room = '';
+
 app.post('/newchat', function(req, res){
 
     console.log(req.body)
@@ -12,7 +14,27 @@ app.post('/newchat', function(req, res){
     var ret;
 
   io.on("connection", client =>{
+
       client.join(req.body.room);
+
+      room = req.body.room;
+
+    client.on(req.body.room, name =>{
+          clients[client.id] = name;
+          client.emit("update", "You have connected to the server.");
+          client.broadcast.emit("update", name + " has joined the server.");
+    });
+      
+    client.on("send", function(msg){
+    	console.log("Message: " + msg);
+        client.emit(room, clients[client.id], msg);
+    });
+
+    client.on("disconnect", function(){
+    	console.log("Disconnect");
+        io.emit("update", clients[client.id] + " has left the server.");
+        delete clients[client.id];
+    });
   })
 
   console.log(ret);
@@ -22,7 +44,7 @@ app.post('/newchat', function(req, res){
 
 io.on("connection", function (client) {  
 
-    client.on("join", function(name){
+    client.on(room, function(name){
     	console.log("Joined: " + name);
         clients[client.id] = name;
         client.emit("update", "You have connected to the server.");
@@ -31,7 +53,7 @@ io.on("connection", function (client) {
 
     client.on("send", function(msg){
     	console.log("Message: " + msg);
-        client.broadcast.emit("chat", clients[client.id], msg);
+        client.emit(room, clients[client.id], msg);
     });
 
     client.on("disconnect", function(){
