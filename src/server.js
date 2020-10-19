64c3f500526302baca5f97a9bec44c1a3b6a096c
XandRules <alexandresilva58@gmail.com>
2020-10-19 16:36:10 -0300

    up

--- src/server.js
+++ src/server.js
@@ -5,43 +5,6 @@ var io = require('socket.io')(http);
 
 var clients = {}; 
 
-var room = '';
-
-app.post('/newchat', function(req, res){
-
-    console.log(req.body)
-
-    var ret;
-
-  io.on("connection", client =>{
-
-      client.join(req.body.room);
-
-      room = req.body.room;
-
-    client.on(req.body.room, name =>{
-          clients[client.id] = name;
-          client.emit("update", "You have connected to the server.");
-          client.broadcast.emit("update", name + " has joined the server.");
-    });
-      
-    client.on("send", function(msg){
-    	console.log("Message: " + msg);
-        client.emit(room, clients[client.id], msg);
-    });
-
-    client.on("disconnect", function(){
-    	console.log("Disconnect");
-        io.emit("update", clients[client.id] + " has left the server.");
-        delete clients[client.id];
-    });
-  })
-
-  console.log(ret);
-    
-  return res.json({room : req.body.room});
-});
-
 io.on("connection", function (client) {  
 
     client.on("join", function(name){
