4563e677ef2355ec9cde5e312d1d7beadcd24268
XandRules <alexandresilva58@gmail.com>
2020-10-28 08:13:26 -0300

    up

--- src/config/mail.js
+++ src/config/mail.js
@@ -12,8 +12,8 @@ export default {
       "user": "xandrules@gmail.com",
       "clientId": "128826042594-0b0ade1t7o4afj4fcl629tvr6irqt48t.apps.googleusercontent.com",
       "clientSecret": "onRadfB7cixkdg87h3VcQKHN",
-      "refreshToken": "1//04vvbZqtk0GRvCgYIARAAGAQSNgF-L9Ir0cLG_3LL-Tt0lQoCENZlTv_qMPDpdfbvHKh74-S_V78S0DxyQAOUtQSZ2DLvfM_Dsg",
-      "accessToken": "ya29.A0AfH6SMDvFDv_NmyqB-WfAxv8fyPSTJ6WXCbY1E-Rm2GosA_tc3ZIAvsVTsrN6nVl7RNssUXGUQfLO1RqXRVrGyj2CZ6cH6hOEBk5-AqWBigH6iVau3IRwN0lqZCAWUnDg7RkKa3Ua2EuQAS5SDSAGgEAvmB5JCjbew71NZzKUOY"
+      "refreshToken": "1//040uoEvSmCE1VCgYIARAAGAQSNgF-L9Irgf20IXbWETKZPIKJ5fvTBuh8hmRYD8M3smecrGaRUA2gyRcDIY5YvJctYoGFp3DYQw",
+      "accessToken": "ya29.A0AfH6SMA0nvXoDI0OUoUN7Z3MC5N7wZ9O4XNIF5_XZw8iznqbfp1AbwBfYlRaIPQAZAKj6A7-_q6qKfiPcp07R96wSG6WBg84-BWWi-BdI9f6a7SUfEyQLZMURp3Ti5qorV3hc7hvaBuAea3q04wKu0drPmlXmgIY3Y-GNKx7mK8"
     }
   }
 };
