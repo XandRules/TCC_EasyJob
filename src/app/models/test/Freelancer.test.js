d0491b74459c794cdda11ef2940c9d6c3ae5da60
XandRules <alexandresilva58@gmail.com>
2020-11-24 11:36:40 -0200

    add teste de create freelancer

--- src/app/models/test/Freelancer.test.js
+++ src/app/models/test/Freelancer.test.js
@@ -3,7 +3,6 @@ module.exports = function(sequelize, DataTypes) {
   return sequelize.define('freelancer', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-<<<<<<< HEAD
     password_hash: DataTypes.STRING,
     phone: DataTypes.STRING,
     cpf: DataTypes.STRING,
@@ -11,18 +10,9 @@ module.exports = function(sequelize, DataTypes) {
     birth: DataTypes.STRING,
     gender: DataTypes.STRING,
     active: DataTypes.STRING,
-    terms_of_use: DataTypes.STRING,
-    password: DataTypes.VIRTUAL,
+    terms_of_use: DataTypes.STRING,    
     speciality_id: DataTypes.NUMBER,
     avatar_id: DataTypes.NUMBER,
     id_hash: DataTypes.STRING,
-=======
-    gender: DataTypes.STRING,
-    birth: DataTypes.STRING,
-    cpf: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    password: DataTypes.STRING,
-    active: DataTypes.BOOLEAN
->>>>>>> add classes de teste
   });
 };
