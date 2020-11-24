ed121942d6aaae1f4fabee4339d427ffaf3cc21e
XandRules <alexandresilva58@gmail.com>
2020-11-24 11:27:37 -0200

    add novos teste unitarios

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 process.env.NODE_ENV = "test";
 
-<<<<<<< HEAD
 describe("Speciality",async  function() {
   let Speciality = require("../src/app/models/test/").speciality;
 
@@ -16,33 +15,12 @@ describe("Speciality",async  function() {
     Speciality.create({ speciality_function: 'Garçom' }).then(function(speciality) {
        console.log(speciality.speciality_function);
       expect(speciality.speciality_function).to.be('Garçom');
-=======
-describe("Speciality", function() {
-  let Speciality = require("../src/app/models/test/").speciality;
-
-  beforeEach(function() {
-    Speciality.sync();
-  });
-
-  afterEach(function() {
-    Speciality.destroy({ truncate: true });
-  });
-
-  it("create", function() {
-    Speciality.create({ speciality_function: 'especialidade' }).then(function(speciality) {
-        //console.log(speciality);
-      expect(speciality.speciality_function).to.be('especialidade');
->>>>>>> add classes de teste
     });
   });
 
 });
 
-<<<<<<< HEAD
 describe("Freelancer", async function() {
-=======
-describe("Freelancer", function() {
->>>>>>> add classes de teste
     let Freelancer = require("../src/app/models/test/").freelancer;
   
     beforeEach(function() {
@@ -50,25 +28,17 @@ describe("Freelancer", function() {
     });
   
     afterEach(function() {
-<<<<<<< HEAD
         Freelancer.destroy({ truncate : true, cascade: false });
     });
 
     let data = {
         "id_hash":"adkasldkALSDa4sd4A4SADa7asdAADas4d5645A4S5D454asd",
-=======
-        Freelancer.destroy({ truncate: true });
-    });
-
-    let data = {
->>>>>>> add classes de teste
         "name": "Alexandre Ribeiro",
         "email":"alexandre@email.com",
         "gender": "Masculino",
         "birth": "07/01/1991",
         "cpf": "12345788911",
         "phone": "99995648999",
-<<<<<<< HEAD
         "password_hash": "123456",
         "bio": "",
         "active":	true,
@@ -102,13 +72,10 @@ describe("Freelancer", function() {
         "social_reason": "Bar e Pizzaria José da Silva",       
         "cnpj": "12345788911",
         "phone": "99995648999",
-=======
->>>>>>> add classes de teste
         "password": "123456",
         "active":	true
     }
   
-<<<<<<< HEAD
    await it("create", function() {
        Establishment.create(data).then(function(establishment) {
         expect(establishment.id).to.be(data);
@@ -170,12 +137,6 @@ describe("Freelancer", function() {
        Announcement.findAll({where : {id: 1}}).then(function(announcement) {  
         console.log(announcement);         
         expect(announcement.id).to.be(data);
-=======
-    it("create", function() {
-        Freelancer.create(data).then(function(freelancer) {
-          console.log(freelancer);
-        expect(freelancer.id).to.be(data);
->>>>>>> add classes de teste
       });
     });
   
