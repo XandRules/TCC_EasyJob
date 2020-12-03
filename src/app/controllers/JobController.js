e16e81860f6f8aef8f6f6d595c8de2a5a5e7a8bf
XandRules <alexandresilva58@gmail.com>
2020-12-03 18:15:03 -0200

    create de jobs

--- src/app/controllers/JobController.js
+++ src/app/controllers/JobController.js
@@ -15,52 +15,57 @@ class JobController {
   }
 
   async store(req, res) {
+
+    console.log("caiu em job")
     const schema = Yup.object().shape({
       freelancer_evaluation: Yup.number(),
       freelancer_comment: Yup.string(),
       establishment_evaluation: Yup.number(),
       establishment_comment: Yup.string(),
       date: Yup.date().required(),
-      freelancer_id: Yup.number().required(),
+      freelancer_id: Yup.number(),
       establishment_id: Yup.number().required(),
       announcement_id: Yup.number().required(),
+      initial_job_id: Yup.number().required(),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json('Validation fail');
     }
 
-    const { date, freelancer_id, establishment_id, announcement_id } = req.body;
+    const { date, establishment_id, announcement_id , initial_job_id } = req.body;
 
-    const isFreelancer = await Freelancer.findOne({
-      where: { id: freelancer_id, active: true, email: req.userEmail },
-    });
+    // const isFreelancer = await Freelancer.findOne({
+    //   where: { id: freelancer_id, active: true, email: req.userEmail },
+    // });
 
-    if (!isFreelancer) {
-      return res.status(401).json('Freelancer not Found');
-    }
+    // if (!isFreelancer) {
+    //   return res.status(401).json('Freelancer not Found');
+    // }
 
-    const isEstablishment = await Establishment.findOne({
-      where: { id: establishment_id, active: true },
-    });
+    // const isEstablishment = await Establishment.findOne({
+    //   where: { id: establishment_id, active: true },
+    // });
 
-    if (!isEstablishment) {
-      return res.status(401).json('Establishment not Found');
-    }
+    // if (!isEstablishment) {
+    //   return res.status(401).json('Establishment not Found');
+    // }
 
-    const isAnnoucement = await Announcement.findOne({
-      where: { id: announcement_id },
-    });
+    // const isAnnoucement = await Announcement.findOne({
+    //   where: { id: announcement_id },
+    // });
 
-    if (!isAnnoucement) {
-      return res.status(401).json('Announcement not Found');
-    }
+    // if (!isAnnoucement) {
+    //   return res.status(401).json('Announcement not Found');
+    // }
 
-    const hourStart = startOfHour(parseISO(date));
+    console.log("passou da Validação");
 
-    if (isBefore(hourStart, new Date())) {
-      return res.status(400).json({ error: 'Past dates are not permitted' });
-    }
+    // const hourStart = startOfHour(parseISO(date));
+
+    // if (isBefore(hourStart, new Date())) {
+    //   return res.status(400).json({ error: 'Past dates are not permitted' });
+    // }
 
     const job = await Job.create(req.body);
 
